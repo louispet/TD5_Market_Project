9ccaa535221dfb297bea4d4e3866a19e17a8723b
Louis <louis.peters@opc.fr>
2021-03-04 19:50:06 +0100

    book avec affichage des df

--- book.py
+++ book.py
@@ -1,43 +1,56 @@
 #!/usr/bin/env python3
 import pandas
 class Order():
+    """
+    La classe ordre ...
+    """
     cpt=1
+    
     # Définition des variables
+    
     def __init__(self,quantity,price,buy=True):
         self.quantity=quantity
         self.price=price
         self.buy=buy
         self.id=Order.cpt
         Order.cpt+=1
+    
     def __eq__(self,other):
         #return other and self.quantity == other.quantity and self.price==other.price
         return other and self.price==other.price
+    
     def __lt__(self,other):
         return other and self.price<other.price
         # Affichage de l'ordre
+    
     def __str__(self,hist=0):
         return ("ID %i -- %i stocks @ price %d  " %(self.id,self.quantity,self.price))
+    
     def __del__(self):
         return 0
 
 
 class Book(Order):
+    
     def __init__(self,name):
         self.__name=name
         self.bookbuy=[]
         self.booksell=[]
+    
     def insert_buy(self,quantity,price):
         insert=Order(quantity,price)
         self.bookbuy.append(insert)
         print ("-- INSERT BUY  -- " , insert)
         self.verif_ordre()
         print(self)
+    
     def insert_sell(self,quantity,price,sell=False):
         insert=Order(quantity,price,False)
         self.booksell.append(insert)
         print("-- INSERT SELL -- " , insert)
         self.verif_ordre()
         print(self)
+    
     def __str__(self):
         df1=pandas.DataFrame(columns=["Type","Price","Quantity"])
         df2=pandas.DataFrame(columns=["Type","Price","Quantity"])
@@ -51,12 +64,12 @@ class Book(Order):
            df1=df1.append({"Type":"BUY","Price":self.bookbuy[n].price,"Quantity":self.bookbuy[n].quantity},ignore_index=True)                                
         df1.index=a
         df2.index=b
-        print(df1)
-        print(df2)
-        return ""
+        return (str(df2)+"\n"+str(df1))
+    
     def sort(self):
         self.bookbuy= sorted(self.bookbuy,key=lambda Order: Order.price, reverse=True)
         self.booksell=sorted(self.booksell,key=lambda Order: Order.price)
+    
     def verif_ordre(self):
         self.sort()
         i=0
@@ -66,6 +79,7 @@ class Book(Order):
                     self.execute_order(i)
                 else:
                     break
+    
     def execute_order(self,i):
         if (self.bookbuy[i].quantity<self.booksell[i].quantity):
             print("Execute order to %i share in %d price" %(self.bookbuy[i].quantity,self.bookbuy[i].price))
