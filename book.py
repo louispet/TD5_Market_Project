380489cf6b25c234f07149becaf7aa12ed5cdb85
Louis <louis.peters@opc.fr>
2021-03-11 16:43:19 +0100

    Adding docstrings

--- book.py
+++ book.py
@@ -2,13 +2,17 @@
 import pandas
 class Order():
     """
-    La classe ordre ...
+    the Order class create an object order.
+    This class is the content of the book. .
     """
     cpt=1
     
     # Définition des variables
     
     def __init__(self,quantity,price,buy=True):
+        """Here we create an object Order, it will be buy or sell order
+        An Order is defined by a quantity of stock / A price (buy/sell) and a type 
+        """
         self.quantity=quantity
         self.price=price
         self.buy=buy
@@ -16,28 +20,38 @@ class Order():
         Order.cpt+=1
     
     def __eq__(self,other):
-        #return other and self.quantity == other.quantity and self.price==other.price
-        return other and self.price==other.price
+       """return if two Orders are equals (override function)"""
+       return other and self.price==other.price
     
     def __lt__(self,other):
+        """Return if two orders are < or > or = (override function)
+        """
         return other and self.price<other.price
         # Affichage de l'ordre
     
     def __str__(self,hist=0):
+        """Override str function for printing content of one object Order
+        """
         return ("ID %i -- %i stocks @ price %d  " %(self.id,self.quantity,self.price))
     
     def __del__(self):
+        """function deleting an instance of order"""
         return 0
 
 
 class Book(Order):
-    
+    """One book is defined by two df of orders (one side sell and the other one buy)
+    """
     def __init__(self,name):
+       
         self.__name=name
         self.bookbuy=[]
         self.booksell=[]
     
     def insert_buy(self,quantity,price):
+        """
+        Here we adding an order in the order book and we check the state of the order book
+        """
         insert=Order(quantity,price)
         self.bookbuy.append(insert)
         print ("-- INSERT BUY  -- " , insert)
@@ -45,6 +59,9 @@ class Book(Order):
         print(self)
     
     def insert_sell(self,quantity,price,sell=False):
+        """
+        Here we adding an order in the order book and we check the state of the order book
+        """
         insert=Order(quantity,price,False)
         self.booksell.append(insert)
         print("-- INSERT SELL -- " , insert)
@@ -52,6 +69,8 @@ class Book(Order):
         print(self)
     
     def __str__(self):
+        """Override of the printing function str for an instance of this class
+        """
         df1=pandas.DataFrame(columns=["Type","Price","Quantity"])
         df2=pandas.DataFrame(columns=["Type","Price","Quantity"])
         a=[]
@@ -60,17 +79,21 @@ class Book(Order):
             b.append(self.booksell[n].id)
             df2=df2.append({"Type":"SELL","Price":self.booksell[n].price,"Quantity":self.booksell[n].quantity},ignore_index=True)
         for n in range(len(self.bookbuy)):
-           a.append(self.bookbuy[n].id)
-           df1=df1.append({"Type":"BUY","Price":self.bookbuy[n].price,"Quantity":self.bookbuy[n].quantity},ignore_index=True)                                
+            a.append(self.bookbuy[n].id)
+            df1=df1.append({"Type":"BUY","Price":self.bookbuy[n].price,"Quantity":self.bookbuy[n].quantity},ignore_index=True)                                
         df1.index=a
         df2.index=b
         return (str(df2)+"\n"+str(df1))
     
     def sort(self):
+        
         self.bookbuy= sorted(self.bookbuy,key=lambda Order: Order.price, reverse=True)
         self.booksell=sorted(self.booksell,key=lambda Order: Order.price)
     
     def verif_ordre(self):
+        """
+        Here we check if we can execute orders
+        """
         self.sort()
         i=0
         if (len(self.bookbuy)!=0 and len(self.booksell)!=0):
@@ -81,6 +104,7 @@ class Book(Order):
                     break
     
     def execute_order(self,i):
+        """This function execute a transaction"""
         if (self.bookbuy[i].quantity<self.booksell[i].quantity):
             print("Execute order to %i share in %d price" %(self.bookbuy[i].quantity,self.bookbuy[i].price))
             temp=self.bookbuy[i].quantity
--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 from book import Book
 
 def main():
+    """
+    Main function of this project. Here we create a book object and we add several orders inside.
+    At each step, book will be print
+    """
     book=Book("TEST")
     book.insert_buy(10,10.0)
     book.insert_sell(120,12.0)
